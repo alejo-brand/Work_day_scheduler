382c26b12c3b740a91c05f2c2e3100cb0416c82f
Carlos Brand <carlosbrand@Carloss-iMac.local>
2020-11-09 13:45:47 -0800

    local storage and display saved info fixed

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+//this function displays the current day and the month with the corresponding cardinal number
 function displayDay(now=moment()){
     $("#currentDay").text(now.format("dddd, MMMM Do"));
 
@@ -7,18 +8,23 @@ function displayDay(now=moment()){
 }
 displayDay();
 
+//this function allows to move through the number of time slots and change the color if the hour is past, present or it's to come//
 function updateTimeCells(now=moment()){
     var actualHour = now.hour();
     console.log(actualHour);
 
     for (i = 9; i<=17; i++){
         if (actualHour>i){
+            //we are using jquery to select the class with id = txt + i which is the variable created to generate the iterations
             $("#txt"+i).addClass("past");
         }else if (actualHour ==i){
             $("#txt" + i).addClass("present");
         }else if (actualHour<i){
             $("#txt" + i).addClass("future");
         }
+        //this allows us to get the value stored in the local Storage using the key name and loading it up again in the text area, in case that the page gets refresehed//
+        var aptSaved = localStorage.getItem("scheduled-hour-" + i)
+        $("#txt" + i).text(aptSaved);
     }
     
     
@@ -29,7 +35,7 @@ updateTimeCells();
 
 
 $(".container").on("click", "button", storage );
-
+//this function listens to a click that happens at the children elements 'buttons' at the parent level and allows us to get the values from that event and text input and store them in local storage.//
 function storage(event){
     event.preventDefault();
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Work_day_scheduler
+
+In this app I develop a work day scheduler that allows the user to organize their work day on a 9am to 5pm schedule.
+
+the past hour time slots will be colored in grey, the current hour will be colored in red and the hours to come will be colored in green.
+
+on each time slot, the user will be able to type all the activities they want to schedule for that specific time and save changes in the browser local storage.
+
+Everytime that the user clicks the save button, it will cause that the app will save all the content provided in the text area for that specific hour, but the information saved will remain displayed in the text area even if the user refreshes the browser page.
+
+below you can find a sample of the deployed application.
